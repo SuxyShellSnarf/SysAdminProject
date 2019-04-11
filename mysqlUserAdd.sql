b18b896f4570a25b730adf4ac35b14cbebeaddd7
Mitchell Parker <mitchellparker@oakland.edu>
2019-04-11 15:39:46 -0400

    Adding stuff

--- mysqlUserAdd.sql
+++ mysqlUserAdd.sql
@@ -26,7 +26,7 @@ insert into website (title, tag, text, file) values ("Team Members and Roles", "
 </ul>&nbsp;
 <h3>Team Roles</h3>
 <p class='text-left' style='text-indent: 40px'>Vince took over as Domain Controller, since he had more knowledge and experience in this type of environment. He handled most of the heavy lifting and configuration for our project.
-  Dante assisted Vince and helped him implement our features.
+  <br/>Dante assisted Vince and helped him implement our features.
   Mitchell worked on creating the website.
 ", "team.php");
 
