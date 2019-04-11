353b059c0a39f499cc7398dfc28278974aaa255c
Mitchell Parker <mitchellparker@oakland.edu>
2019-04-11 15:38:34 -0400

    Adding stuff

--- mysqlUserAdd.sql
+++ mysqlUserAdd.sql
@@ -5,18 +5,18 @@ create table website (
 content_id int unsigned auto_increment primary key,
 title varchar(255),
 tag varchar(255),
-text text,
+text longtext,
 file varchar(255)
 );
 
 insert into website (title, tag, text, file) values ("Project Overview", "overview", "<h3>Overview</h3>
-<p class="text-left" style="text-indent: 40px">We are implementing Single Sign On.
+<p class='text-left' style='text-indent: 40px'>We are implementing Single Sign On.
 
 We have a working build with LDAP in the sense that an active directory user can log into nextcloud using their AD information.
 
 We have ADFS kinda working. We still have some errors to work through though.</p>", "index.php");
 
-insert into website (title, tag, text, file) values ("Screenshots", "sc", "<p style="text-indent: 40px">This is a list of all of our gathered screenshots.</p>", "screenshots.php");
+insert into website (title, tag, text, file) values ("Screenshots", "sc", "<p class='text-left' style='text-indent: 40px'>This is a list of all of our gathered screenshots.</p>", "screenshots.php");
 
 insert into website (title, tag, text, file) values ("Team Members and Roles", "team", "<h3>Team Members</h3>
 <ul>
@@ -24,8 +24,13 @@ insert into website (title, tag, text, file) values ("Team Members and Roles", "
 <li>Dante Gennero
 <li>Mitchell Parker
 </ul>&nbsp;
+<h3>Team Roles</h3>
+<p class='text-left' style='text-indent: 40px'>Vince took over as Domain Controller, since he had more knowledge and experience in this type of environment. He handled most of the heavy lifting and configuration for our project.
+  Dante assisted Vince and helped him implement our features.
+  Mitchell worked on creating the website.
 ", "team.php");
 
 insert into website (title, tag, text, file) values ("How to Access", "access", "Some more text", "access.php");
 
 insert into website (title, tag, text, file) values ("How to Setup", "setup", "And yet some more", "setup.php");
+
