89cc0eef83965aacd2ce838086b0d48cc6ea6a43
Mitchell Parker <mitchellparker@oakland.edu>
2019-04-11 14:53:24 -0400

    Adding pages

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ $sql = "select * from website where tag = 'overview'";
 $stmt = $db->prepare($sql);
 $stmt->execute();
 $text = $stmt->fetch(PDO::FETCH_ASSOC);
-echo print_r($text, true);
 ?>
 
 <!DOCTYPE html>
