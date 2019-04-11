84b85745c7fe26bd9e65984a2fb0b17aba27ace1
Mitchell Parker <mitchellparker@oakland.edu>
2019-04-11 14:20:37 -0400

    Adding pages

--- index.php
+++ index.php
@@ -39,6 +39,17 @@ require "db.php" ?>
                 $text = $stmt->fetch(PDO::FETCH_ASSOC);
                 echo $text["text"];
                 ?></p>
+
+            <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Directory
+                    <span class="caret"></span></button>
+                <ul class="dropdown-menu">
+                    <li><a href="#">HTML</a></li>
+                    <li><a href="#">CSS</a></li>
+                    <li><a href="#">JavaScript</a></li>
+                </ul>
+            </div>
+            
         </div>
         <div class="col-3">
         </div>
