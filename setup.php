ba33de210679fbd93bdc0673f33b34120f356371
SuxyShellSnarf <mparker95@yahoo.com>
2019-04-18 00:33:58 -0400

    Adding junk

--- setup.php
+++ setup.php
@@ -34,7 +34,7 @@ $text = $stmt->fetch(PDO::FETCH_ASSOC);
     <div class="row text-center">
         <div class="col-2">
         </div>
-        <div class="col-8">
+        <div class="col-8" style="text-align: left;">
             <?php echo $text["text"]; ?>
 
             <div class="dropdown">
